9e367350fca227ce751be811f0aec2f6ccbec412
Justin.s.s <jsabia@clemson.edu>
2023-11-30 10:41:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,84 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Fetch the CSV file
-    fetch('data.csv')
-        .then(response => response.text())
-        .then(data => {
-            // Convert CSV to array of objects
-            const players = CSVtoJSON(data);
+    // Assuming you have the data in the same order as the players
+    const playerData = [
+        'Karl-Anthony Towns,37',
+        'Stephen Curry,34.2',
+        'Luka Dončić,33.2',
+        'Giannis Antetokounmpo,32.7',
+        'Joel Embiid,31.7',
+        'Kevin Durant,29.3',
+        'Bradley Beal,29.2',
+        'LeBron James,29',
+        'Nikola Jokić,26.4',
+        'Trae Young,25.3',
+        'Devin Booker,25.1',
+        'James Harden,25',
+        'Zach LaVine,24.1',
+        'Ja Morant,24',
+        'Malcolm Brogdon,21.2',
+        'Fred VanVleet,20.2',
+        'DeMar DeRozan,19.7',
+        'CJ McCollum,19.5',
+        'Chris Duarte,19.4',
+        'De'Aaron Fox,19.3',
+        'Norman Powell,19',
+        'Khris Middleton,18.8',
+        'Terry Rozier,18.7',
+        'Shai Gilgeous-Alexander,18.6',
+        'Kyle Lowry,17.4'
+    ];
 
-            // Extract player names and points for the bar chart
-            const playerNames = players.map(player => player.Player);
-            const points = players.map(player => parseFloat(player.Points));
+    const playerNames = playerData.map(entry => entry.split(',')[0]);
+    const playerPoints = playerData.map(entry => parseFloat(entry.split(',')[1]));
 
-            // Create bar chart
-            const ctx = document.getElementById('barChart').getContext('2d');
-            new Chart(ctx, {
-                type: 'bar',
-                data: {
-                    labels: playerNames,
-                    datasets: [{
-                        label: 'NBA Player Points',
-                        data: points,
-                        backgroundColor: 'rgba(75, 192, 192, 0.5)',
-                        borderColor: 'rgba(75, 192, 192, 1)',
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        }
-                    }
+    // Bar Chart
+    const ctx = document.getElementById('barChart').getContext('2d');
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: playerNames,
+            datasets: [{
+                label: 'Points',
+                data: playerPoints,
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
                 }
-            });
-        })
-        .catch(error => console.error('Error fetching the CSV file:', error));
-
-    // Function to convert CSV to JSON
-    function CSVtoJSON(csv) {
-        const lines = csv.split('\n');
-        const result = [];
-        const headers = lines[0].split(',');
-
-        for (let i = 1; i < lines.length; i++) {
-            const obj = {};
-            const currentline = lines[i].split(',');
-
-            for (let j = 0; j < headers.length; j++) {
-                obj[headers[j]] = currentline[j];
             }
-
-            result.push(obj);
         }
+    });
 
-        return result;
-    }
+    // Add code to create another chart on the second canvas (secondChart)
+    // ...
+
+    // Scatter Plot (just to check)
+    const scatterCtx = document.getElementById('scatterChart').getContext('2d');
+    new Chart(scatterCtx, {
+        type: 'scatter',
+        data: {
+            labels: playerNames,
+            datasets: [{
+                label: 'Points',
+                data: playerPoints,
+                backgroundColor: 'rgba(255, 99, 132, 1)'
+            }]
+        },
+        options: {
+            scales: {
+                x: {
+                    type: 'linear',
+                    position: 'bottom'
+                },
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
 });
